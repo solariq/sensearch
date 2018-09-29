fba44235f3854b79cdaa250845b8a571062702d0
Anton <anton1321@mail.ru>
2018-09-29 10:59:06 +0300

    base Maven project

---
+++ simpleSearch/src/main/java/simpleSearch/App.java
@@ -0,0 +1,13 @@
+package simpleSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
