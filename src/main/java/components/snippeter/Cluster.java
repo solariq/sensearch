d3149ff2316566ddba92f0dcd52f281096e742ed
AlvinMax <alvinmax@mail.ru>
2018-10-10 19:20:24 +0300

    add Cluster and Passage

---
+++ src/main/java/components/snippeter/Cluster.java
@@ -0,0 +1,23 @@
+package components.snippeter;
+
+import java.util.List;
+
+/**
+ * Created by Maxim on 10.10.2018.
+ * Email: alvinmax@mail.ru
+ */
+public class Cluster {
+    List<Passage> passages;
+    long rating;
+
+    public Cluster(List<Passage> passages) {
+        this.passages = passages;
+        rating = passages.stream().mapToLong(Passage::getRating).sum();
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+
+}
---
+++ src/main/java/components/snippeter/Passage.java
@@ -0,0 +1,37 @@
+package components.snippeter;
+
+import components.query.Query;
+
+/**
+ * Created by Maxim on 10.10.2018.
+ * Email: alvinmax@mail.ru
+ */
+public class Passage {
+    CharSequence sentence;
+    long rating;
+
+    public Passage(CharSequence sentence, Query query) {
+        this.sentence = sentence;
+        rating = query.getTerms().stream().filter(x -> contains(sentence, x.getRaw())).count();
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public CharSequence getSentence() {
+        return sentence;
+    }
+
+    private boolean contains(CharSequence s, CharSequence t) {
+        int n = s.length();
+        int m = t.length();
+
+        for (int i = 0; i < n - m + 1; ++i) {
+            if (s.subSequence(i , i + m).equals(t)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
