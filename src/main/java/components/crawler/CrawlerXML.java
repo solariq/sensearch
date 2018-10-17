c25ac02702e78752502cefd7d429fff0eb895713
Artem Abramov <atem199700@gmail.com>
2018-10-17 18:23:57 +0300

    Del useless methods

--- src/main/java/components/crawler/CrawlerXML.java
+++ src/main/java/components/crawler/CrawlerXML.java
@@ -18,7 +18,6 @@ import java.util.zip.ZipInputStream;
 public class CrawlerXML implements Crawler {
 
     private Path path;
-    private XMLParser parser = new XMLParser();
 
     public CrawlerXML(Path path) {
         this.path = path;
@@ -30,49 +29,6 @@ public class CrawlerXML implements Crawler {
                 Spliterators.spliteratorUnknownSize(new DocumentIterator(path),
                         Spliterator.ORDERED | Spliterator.SORTED),
                 false);
-    }
-
-    @Override
-    public CrawlerDocument getDocument(long iD) {
-        return runLib(iD);
-    }
-
-    private CrawlerDocument runLib(long id) {
-        try {
-            FileInputStream fileInputStream =
-                    new FileInputStream(path.toString());
-            ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
-            ZipEntry zipEntry;
-            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                if (zipEntry.getName().equals("Split_Wiki/" + Long.toString(id) + ".xml")) {
-
-                    String fileName = "../WikiDocs/tmp/";
-                    Files.createDirectories(Paths.get(fileName));
-                    fileName += Long.toString(id) + ".xml";
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-
-                    //Need refactor
-                    for (int c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
-                        fileOutputStream.write(c);
-                    }
-
-                    fileOutputStream.flush();
-                    zipInputStream.closeEntry();
-                    fileOutputStream.close();
-                    File file = new File(fileName);
-
-                    CrawlerDocument result = parser.parseXML(file);
-
-                    file.deleteOnExit();
-
-                    return result;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
 
@@ -112,7 +68,7 @@ class DocumentIterator implements Iterator<CrawlerDocument> {
     @Override
     public CrawlerDocument next() {
         try {
-             while (zipEntry.isDirectory()) {
+            while (zipEntry.isDirectory()) {
                 zipEntry = nextZipEntry;
                 nextZipEntry = zipInputStream.getNextEntry();
             }
@@ -121,23 +77,22 @@ class DocumentIterator implements Iterator<CrawlerDocument> {
             String[] n = zipEntry.getName().split("/");
             fileName += n[1];
 
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
 
-            //Need refactor
-            for (int c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
-                fileOutputStream.write(c);
+                //Need refactor
+                for (int c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
+                    fileOutputStream.write(c);
+                }
+
+                zipInputStream.closeEntry();
+                File file = new File(fileName);
+
+                CrawlerDocument result = parser.parseXML(file);
+
+                file.delete();
+
+                return result;
             }
-
-            fileOutputStream.flush();
-            zipInputStream.closeEntry();
-            fileOutputStream.close();
-            File file = new File(fileName);
-
-            CrawlerDocument result = parser.parseXML(file);
-
-            file.delete();
-
-            return result;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
