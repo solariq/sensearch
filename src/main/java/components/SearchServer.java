991aa58c0b9cace9dc20fea5b61e5c4ae57d48dc
Artem Abramov <atem199700@gmail.com>
2018-10-20 15:50:37 +0300

    Исправлен README, а так же убранна зависимость путей от системы.

--- src/main/java/components/SearchServer.java
+++ src/main/java/components/SearchServer.java
@@ -6,11 +6,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SearchServer {
-	
+
+    //Путь до архива
 	private static Path pathToZIP = Paths.get("../WikiDocs/Mini_Wiki.zip");
  
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8082);
+        Server server = new Server(8081);
         //server.setHandler(new HandlerCollection(new Handler[] {new PageLoadHandler(), new SuggestionsHandler()}));
         server.setHandler(new PageLoadHandler(pathToZIP));
 
