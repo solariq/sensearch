a40ed998320c76a04b3efff833a732b3cdced7a5
Maxim <m.a.tereshchuk@gmail.com>
2018-11-03 01:29:45 +0300

    Test refactor

--- src/test/java/com/expleague/sensearch/embedding/NearestFinderTest.java
+++ src/test/java/com/expleague/sensearch/embedding/NearestFinderTest.java
@@ -1,39 +1,54 @@
 package com.expleague.sensearch.embedding;
 
-import com.expleague.sensearch.core.Embedding;
-import com.expleague.sensearch.core.NearestFinder;
-import com.expleague.sensearch.core.impl.EmbeddingImpl;
-import com.expleague.sensearch.core.impl.NearestFinderImpl;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.expleague.sensearch.core.impl.EmbeddingImpl;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NearestFinderTest {
 
   private static final int numberOfNeighbors = 50;
 
-  private static final HashMap<String, String[]> tests;
+  private static final Map<String, String[]> tests;
 
   static {
-    HashMap<String, String[]> hashMap = new HashMap<>();
-    hashMap.put("женщина", new String[]{"девушка", "девочка", "молодая", "красивая", "мать"});
-    hashMap.put("вода", new String[]{"пресная", "лёд", "дистиллированная", "солёная", "питьевая"});
-    hashMap.put("школа",
+    Map<String, String[]> map = new HashMap<>();
+    map.put("женщина", new String[]{"девушка", "девочка", "молодая", "красивая", "мать"});
+    map.put("вода", new String[]{"пресная", "лёд", "воздух", "солёная", "питьевая"});
+    map.put("школа",
         new String[]{"гимназия", "начальная", "общеобразовательная", "музыкальная", "спортивная"});
-    tests = hashMap;
+    tests = map;
+  }
+
+  private EmbeddingImpl embedding;
+
+  @Before
+  public void initEmbedding() {
+    embedding = EmbeddingImpl.getInstance();
+  }
+
+  private void nearestFinderTest() {
+    for (Map.Entry<String, String[]> entry : tests.entrySet()) {
+      List<String> nearest = embedding.getNearestWords(embedding.getVec(entry.getKey()), numberOfNeighbors);
+      for (String neighbor : entry.getValue()) {
+        Assert.assertTrue(nearest.contains(neighbor));
+      }
+    }
   }
 
   @Test
-  public void nearestFinderTest() {
-    Embedding embedding = EmbeddingImpl.getInstance();
-    NearestFinder nearestFinder = new NearestFinderImpl();
-    for (HashMap.Entry<String, String[]> e : tests.entrySet()) {
-      for (String neighbor : e.getValue()) {
-        Assert.assertTrue(
-            nearestFinder.getNearestWords(embedding.getVec(e.getKey()), numberOfNeighbors)
-                .contains(neighbor));
-      }
+  public void euclideanTest() {
+    embedding.switchMeasureToEuclidean();
+    nearestFinderTest();
+  }
 
-    }
+  @Test
+  public void cosineTest() {
+    embedding.switchMeasureToCosine();
+    nearestFinderTest();
   }
 }
