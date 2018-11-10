1a312f3c5ed77676009793912f54d28d5173480d
Maxim <m.a.tereshchuk@gmail.com>
2018-11-10 13:26:57 +0300

    Fix FilterTest

--- src/test/java/com/expleague/sensearch/embedding/FilterTest.java
+++ src/test/java/com/expleague/sensearch/embedding/FilterTest.java
@@ -1,18 +1,20 @@
 package com.expleague.sensearch.embedding;
 
+import com.expleague.commons.text.lemmer.LemmaInfo;
 import com.expleague.sensearch.Config;
+import com.expleague.sensearch.Page;
 import com.expleague.sensearch.donkey.crawler.CrawlerXML;
 import com.expleague.sensearch.index.Index;
-import com.expleague.sensearch.index.IndexedPage;
 import com.expleague.sensearch.index.plain.PlainIndexBuilder;
-import com.expleague.sensearch.query.BaseQuery;
 import com.expleague.sensearch.query.Query;
+import com.expleague.sensearch.query.term.Term;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,13 +26,13 @@ public class FilterTest {
 
     static {
         Map<String, String[]> map = new HashMap<>();
-        map.put("музыка России", new String[]{"музыка", "песня", "Россия", "русская", "русский"});
+        map.put("музыка России", new String[]{"музыка", "песня", "России", "русская", "русский"});
         tests = map;
     }
 
     @Before
     public void initIndex() throws Exception {
-        byte[] jsonData = Files.readAllBytes(Paths.get("./paths.json"));
+        byte[] jsonData = Files.readAllBytes(Paths.get("./config.json"));
         Config config = new ObjectMapper().readValue(jsonData, Config.class);
         index = new PlainIndexBuilder(config).buildIndex(new CrawlerXML(config).makeStream());
     }
@@ -38,9 +40,47 @@ public class FilterTest {
     @Test
     public void filtrateTest() {
         for (Map.Entry<String, String[]> entry : tests.entrySet()) {
-            Query query = new BaseQuery(entry.getKey());
-            Stream<IndexedPage> pageStream = index.fetchDocuments(query);
-            pageStream.forEach(indexedPage -> System.out.println(indexedPage.title()));
+            Query query = new QueryStub(entry.getKey());
+            Stream<Page> pageStream = index.fetchDocuments(query);
+            Assert.assertTrue(pageStream.anyMatch(p -> Arrays.stream(entry.getValue()).anyMatch(w -> p.title().toString().contains(w))));
+        }
+    }
+
+    private static class QueryStub implements Query {
+        private List<Term> terms;
+        QueryStub(String query) {
+            terms = new LinkedList<>();
+            for (String w : query.split("[^А-ЯЁа-яёA-Za-z0-9]")) {
+                terms.add(new TermStub(w));
+            }
+        }
+
+        @Override
+        public List<Term> getTerms() {
+            return terms;
+        }
+    }
+
+    private static class TermStub implements Term {
+        private String rawTerm;
+
+        TermStub(String term) {
+            rawTerm = term;
+        }
+
+        @Override
+        public CharSequence getRaw() {
+            return rawTerm;
+        }
+
+        @Override
+        public CharSequence getNormalized() {
+            return null;
+        }
+
+        @Override
+        public LemmaInfo getLemma() {
+            return null;
         }
     }
 }
