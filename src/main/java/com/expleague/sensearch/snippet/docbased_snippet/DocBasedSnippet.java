fcceca93d69ce6701c2973f4d94002e5316ac376
AlviMax <earth.maxim@gmail.com>
2018-11-14 19:10:15 +0300

    snippets <= 300 🚜🚜🚜

--- src/main/java/com/expleague/sensearch/snippet/docbased_snippet/DocBasedSnippet.java
+++ src/main/java/com/expleague/sensearch/snippet/docbased_snippet/DocBasedSnippet.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DocBasedSnippet implements Snippet {
-  private static final long MAX_LENGTH = 300;
+  private static final long MAX_LENGTH = 300; //🚜🚜🚜
 
   private CharSequence title;
   private CharSequence content;
