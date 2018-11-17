682ab48f92fa76c1759ac5b2306e16d673445aa3
Artem Abramov <atem199700@gmail.com>
2018-11-17 13:25:33 +0300

    Added Metric test

--- src/test/java/com/expleague/sensearch/metricTest/LocalRequestCrawler.java
+++ src/test/java/com/expleague/sensearch/metricTest/LocalRequestCrawler.java
@@ -1,20 +1,32 @@
 package com.expleague.sensearch.metricTest;
 
 import com.expleague.sensearch.SenSeArch.ResultItem;
+import com.expleague.sensearch.SenSeArch.ResultPage;
 import com.expleague.sensearch.metrics.WebCrawler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class LocalRequestCrawler implements WebCrawler {
 
-  private final String MAP_FILE = "MAP";
+  private final String MAP_FILE = "PAGE.json";
   private Set<String> allTitles;
   private Path pathToMetric;
+  public String query;
 
   @Override
   public List<ResultItem> getGoogleResults(Integer size, String query) {
-
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      ResultPage page = objectMapper.readValue(pathToMetric.resolve(query).resolve(MAP_FILE).toFile(),
+          ResultPage.class);
+      return Arrays.asList(page.googleResults());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return null;
   }
 
