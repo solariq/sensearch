0efbbba2861cd5b775a3d57db70d5a4e11ef9849
SandMV <sandulmv@yandex.ru>
2018-12-15 12:28:08 +0300

    Add more tests for PageBuilder

--- src/test/java/com/expleague/sensearch/donkey/plain/PlainPageBuilderMethodsTest.java
+++ src/test/java/com/expleague/sensearch/donkey/plain/PlainPageBuilderMethodsTest.java
@@ -3,11 +3,15 @@ package com.expleague.sensearch.donkey.plain;
 import com.expleague.sensearch.donkey.crawler.document.CrawlerDocument;
 import com.expleague.sensearch.donkey.crawler.document.CrawlerDocument.Link;
 import com.expleague.sensearch.donkey.crawler.document.CrawlerDocument.Section;
+import com.expleague.sensearch.donkey.plain.PlainPageBuilderMethodsTest.SectionMock.LinkMock;
+import com.expleague.sensearch.protobuf.index.IndexUnits;
 import com.expleague.sensearch.protobuf.index.IndexUnits.Page;
 import com.expleague.sensearch.utils.SensearchTestCase;
 import com.google.common.collect.Lists;
+import gnu.trove.map.TLongLongMap;
 import gnu.trove.map.TLongObjectMap;
 import gnu.trove.map.TObjectLongMap;
+import gnu.trove.map.hash.TLongLongHashMap;
 import gnu.trove.map.hash.TLongObjectHashMap;
 import gnu.trove.map.hash.TObjectLongHashMap;
 import java.net.URI;
@@ -21,7 +25,9 @@ import org.junit.Test;
 
 public class PlainPageBuilderMethodsTest extends SensearchTestCase {
 
-  private static class SectionMock implements CrawlerDocument.Section {
+  private final static URI DEFAULT_URI = URI.create("https://www.wikipedia.org/");
+
+  static class SectionMock implements CrawlerDocument.Section {
 
     private String text = "";
     private List<Link> links = new ArrayList<>();
@@ -87,7 +93,7 @@ public class PlainPageBuilderMethodsTest extends SensearchTestCase {
     
     @Override
     public URI uri() {
-      return null;
+      return DEFAULT_URI;
     }
 
     SectionMock addLink(Link link) {
@@ -104,12 +110,6 @@ public class PlainPageBuilderMethodsTest extends SensearchTestCase {
       this.title = Lists.newArrayList(title);
       return this;
     }
-  }
-
-
-  @Test
-  public void resolveLinkTest() {
-
   }
 
   @Test
@@ -217,9 +217,64 @@ public class PlainPageBuilderMethodsTest extends SensearchTestCase {
   }
 
   @Test
-  @Ignore
-  // TODO: write test
   public void resolveLinksTest() {
+    List<IndexUnits.Page.Link.Builder> links = new ArrayList<>();
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(11)
+        .setTargetPageId(2)
+        .setText("l.11.12")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(11)
+        .setTargetPageId(3)
+        .setText("l.11.13")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(11)
+        .setTargetPageId(4)
+        .setText("l.11.14")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(12)
+        .setTargetPageId(1)
+        .setText("l.12.11")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(12)
+        .setTargetPageId(4)
+        .setText("l.12.14")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(12)
+        .setTargetPageId(5)
+        .setText("l.12.15")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(14)
+        .setTargetPageId(1)
+        .setText("l.14.11")
+    );
+    links.add(Page.Link.newBuilder()
+        .setSourcePageId(14)
+        .setTargetPageId(3)
+        .setText("l.14.13")
+    );
 
+    TLongLongMap wikiToIndexMappings = new TLongLongHashMap();
+    wikiToIndexMappings.put(1, 11);
+    wikiToIndexMappings.put(2, 12);
+    wikiToIndexMappings.put(3, 13);
+    wikiToIndexMappings.put(4, 14);
+
+    TLongObjectMap<List<IndexUnits.Page.Link>> incomingLinks = new TLongObjectHashMap<>();
+    TLongObjectMap<List<IndexUnits.Page.Link>> outcomingLinks = new TLongObjectHashMap<>();
+    PlainPageBuilder.resolveLinks(links, wikiToIndexMappings, outcomingLinks, incomingLinks);
+
+    Assert.assertEquals(3, outcomingLinks.get(11).size());
+    Assert.assertEquals(3, outcomingLinks.get(12).size());
+    Assert.assertEquals(2, outcomingLinks.get(14).size());
+
+    Assert.assertEquals(2, incomingLinks.get(14).size());
+    Assert.assertEquals(2, incomingLinks.get(11).size());
   }
 }
