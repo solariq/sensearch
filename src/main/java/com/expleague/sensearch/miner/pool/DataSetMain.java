f787affc247264a8303d4eea04f706b1c98eb9bf
Artem Abramov <atem199700@gmail.com>
2018-12-26 20:41:24 +0300

    Fix DataSet

--- src/main/java/com/expleague/sensearch/miner/pool/DataSetMain.java
+++ src/main/java/com/expleague/sensearch/miner/pool/DataSetMain.java
@@ -7,19 +7,26 @@ import com.expleague.ml.meta.DataSetMeta;
 import com.expleague.ml.meta.impl.JsonDataSetMeta;
 import com.expleague.sensearch.AppModule;
 import com.expleague.sensearch.Page;
+import com.expleague.sensearch.SenSeArch.ResultItem;
+import com.expleague.sensearch.core.impl.ResultItemImpl;
 import com.expleague.sensearch.index.Index;
 import com.expleague.sensearch.miner.impl.AccumulatorFeatureSet;
 import com.expleague.sensearch.miner.impl.QURLItem;
 import com.expleague.sensearch.miner.impl.TargetFeatureSet;
 import com.expleague.sensearch.query.BaseQuery;
 import com.expleague.sensearch.query.Query;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class DataSetMain {
@@ -46,10 +53,27 @@ public class DataSetMain {
       while ((line = reader.readLine()) != null) {
         if (Files.exists(Paths.get("./wordstat").resolve("query_" + line))) {
           Query query = BaseQuery.create(line, index);
+          Set<String> uniqQURL = new HashSet<>();
+
+          try (BufferedReader queryReader = Files
+              .newBufferedReader(Paths.get("./wordstat").resolve("query_" + query.text()))) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ResultItem[] res = objectMapper.readValue(queryReader, ResultItemImpl[].class);
+            for(ResultItem page : res) {
+              uniqQURL.add(page.title().toString());
+              poolBuilder.accept(new QURLItem(index.page(page.reference()), query));
+              poolBuilder.advance();
+            }
+          } catch (IOException ignored) {
+          }
+
           Stream<Page> sensearchResult = index.fetchDocuments(query);
-          sensearchResult.map(page -> new QURLItem(page, query)).forEach(item -> {
-            poolBuilder.accept(item);
-            poolBuilder.advance();
+          sensearchResult.forEach(page -> {
+            if (!uniqQURL.contains(page.title().toString())) {
+              uniqQURL.add(page.title().toString());
+              poolBuilder.accept(new QURLItem(page, query));
+              poolBuilder.advance();
+            }
           });
         }
       }
