dba323cafc11828bc3cf3007c93047981563a537
solar <ikuralenok@gmail.com>
2019-01-12 16:28:30 +0300

    A bit of refactoring using VecTools

--- src/main/java/com/expleague/sensearch/miner/impl/CosinusDistanceFeatureSet.java
+++ src/main/java/com/expleague/sensearch/miner/impl/CosinusDistanceFeatureSet.java
@@ -1,15 +1,14 @@
 package com.expleague.sensearch.miner.impl;
 
 import com.expleague.commons.math.vectors.Vec;
+import com.expleague.commons.math.vectors.VecTools;
 import com.expleague.ml.data.tools.FeatureSet;
 import com.expleague.ml.meta.FeatureMeta;
 import com.expleague.ml.meta.FeatureMeta.ValueType;
 
 public class CosinusDistanceFeatureSet extends FeatureSet.Stub<QURLItem> {
-
   public final static FeatureMeta COSINUS_DISTANCE = FeatureMeta
-      .create("cosinus_distance", "cosinus distance between Query and Title", ValueType.VEC);
-
+      .create("cos-title", "cosinus distance between Query and Title", ValueType.VEC);
 
   private Vec queryVec;
   private Vec titleVec;
@@ -21,25 +20,7 @@ public class CosinusDistanceFeatureSet extends FeatureSet.Stub<QURLItem> {
 
   @Override
   public Vec advance() {
-    double result = 0.0;
-    double querySum = 0.0;
-    double titleSum = 0.0;
-
-    for (int i = 0; i < queryVec.dim(); i++) {
-      result += (queryVec.get(i) * titleVec.get(i));
-      querySum += queryVec.get(i) * queryVec.get(i);
-      titleSum += titleVec.get(i) * titleVec.get(i);
-    }
-    querySum = Math.sqrt(querySum);
-    titleSum = Math.sqrt(titleSum);
-    if ((querySum * titleSum) == 0.0) {
-      result = 0;
-    } else {
-      result /= querySum * titleSum;
-    }
-
-    set(COSINUS_DISTANCE, result);
+    set(COSINUS_DISTANCE, 1 - 2 * VecTools.cosine(queryVec, titleVec));
     return super.advance();
   }
-
 }
