d132d20510912480ecda806d6ecdbedb8d872db9
Artem Abramov <atem199700@gmail.com>
2019-04-06 13:21:40 +0300

    Add new format for PoolBuilder

---
+++ src/main/java/com/expleague/sensearch/miner/pool/QueryAndResults.java
@@ -0,0 +1,56 @@
+package com.expleague.sensearch.miner.pool;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.net.URI;
+
+@JsonPropertyOrder({"query", "answers"})
+public class QueryAndResults {
+
+  private String query;
+  private PageAndRank[] answers;
+
+  @JsonCreator
+  public QueryAndResults(@JsonProperty("query") String query
+      , @JsonProperty("answers") PageAndRank[] answers) {
+    this.query = query;
+    this.answers = answers;
+  }
+
+  @JsonProperty("query")
+  public String getQuery() {
+    return query;
+  }
+
+  @JsonProperty("answers")
+  public PageAndRank[] getAnswers() {
+    return answers;
+  }
+
+  @JsonPropertyOrder({"uri", "rank"})
+  public static class PageAndRank {
+
+    private final URI uri;
+    private final double rank;
+
+    @JsonCreator
+    public PageAndRank(@JsonProperty("uri") String uri
+        , @JsonProperty("rank") double rank) {
+      this.uri = URI.create(uri);
+      this.rank = rank;
+    }
+
+    @JsonProperty("uri")
+    public URI getUri() {
+      return uri;
+    }
+
+    @JsonProperty("rank")
+    public double getRank() {
+      return rank;
+    }
+  }
+
+
+}
