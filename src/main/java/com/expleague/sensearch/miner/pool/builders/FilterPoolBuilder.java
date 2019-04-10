96da40ec03b09637d5d14a83654bd71693e7ea75
Artem Abramov <atem199700@gmail.com>
2019-04-10 14:10:45 +0300

    some fix

--- src/main/java/com/expleague/sensearch/miner/pool/builders/FilterPoolBuilder.java
+++ src/main/java/com/expleague/sensearch/miner/pool/builders/FilterPoolBuilder.java
@@ -8,7 +8,6 @@ import com.expleague.commons.random.FastRandom;
 import com.expleague.commons.util.Pair;
 import com.expleague.ml.data.tools.DataTools;
 import com.expleague.ml.data.tools.Pool;
-import com.expleague.ml.data.tools.Pool.Builder;
 import com.expleague.ml.meta.DataSetMeta;
 import com.expleague.ml.meta.FeatureMeta;
 import com.expleague.ml.meta.impl.JsonDataSetMeta;
@@ -81,7 +80,7 @@ public class FilterPoolBuilder extends PoolBuilder {
 
   public static void main(String[] args) throws IOException {
     Injector injector = Guice.createInjector(new AppModule());
-    injector.getInstance(FilterPoolBuilder.class).build(Paths.get("./PoolData/filter/"), 1);
+    injector.getInstance(FilterPoolBuilder.class).build(Paths.get("./PoolData/filter/"), 0);
   }
 
   public void build(Path dir, int iteration) {
@@ -105,59 +104,81 @@ public class FilterPoolBuilder extends PoolBuilder {
             qNr -> Arrays.asList(qNr.getAnswers())));
     List<QueryAndResults> newData = new ArrayList<>();
 
-    data.forEach((query, res) -> {
-      if (status.get() % 100 == 0) {
-        System.err.println(status.get() + " queries completed");
-      }
-      status.incrementAndGet();
-      Map<Page, Features> allDocs =
-          index.fetchDocuments(query, FilterMinerPhase.FILTERED_DOC_NUMBER);
+    data.entrySet().parallelStream()
+        .forEach(entry -> {
+          Query query = entry.getKey();
+          List<PageAndWight> res = entry.getValue();
+          if (status.get() % 100 == 0) {
+            System.err.println(status.get() + " queries completed");
+          }
+          status.incrementAndGet();
+          Map<Page, Features> allDocs =
+              index.fetchDocuments(query, FilterMinerPhase.FILTERED_DOC_NUMBER);
 
-      res.forEach(
-          pNw -> {
-            Page page = index.page(pNw.getUri());
-            if (page != PlainPage.EMPTY_PAGE) {
-              double target = 0;
-              if (pNw.getWight() > 0) {
-                target = 1;
-              }
-              accept(poolBuilder, page, query,
-                  ((PlainIndex) index).filterFeatures(query, page.uri()), target);
-              allDocs.remove(page);
-            }
-          });
+          res.forEach(
+              pNw -> {
+                Page page = index.page(pNw.getUri());
+                if (page != PlainPage.EMPTY_PAGE) {
+                  double target = 0;
+                  if (pNw.getWight() > 0) {
+                    target = 1;
+                  }
 
-      List<PageAndWight> newRes = new ArrayList<>(res);
-      int tmpAdded = added.get();
-      int[] cnt = {0};
-      allDocs
-          .entrySet()
-          .stream()
-          .sorted(Comparator.comparingDouble(e -> -model.trans(e.getValue().features()).get(0)))
-          .forEach(e -> {
-            Page page = e.getKey();
-            Features feat = e.getValue();
-            if (page == PlainPage.EMPTY_PAGE) {
-              return;
-            }
+                  Features feat = ((PlainIndex) index).filterFeatures(query, page.uri());
+                  synchronized (poolBuilder) {
+                    poolBuilder.accept(new QURLItem(page, query));
+                    double finalTarget = target;
+                    poolBuilder
+                        .features()
+                        .forEach(
+                            fs -> {
+                              if (fs instanceof TargetFS) {
+                                ((TargetFS) fs).acceptTargetValue(finalTarget);
+                              } else if (fs instanceof FilterFeatures) {
+                                ((FilterFeatures) fs).withBody(feat.features(SECTION).get(0));
+                                ((FilterFeatures) fs).withLink(feat.features(LINK).get(0));
+                                ((FilterFeatures) fs).withTitle(feat.features(TITLE).get(0));
+                              }
+                            }
+                        );
+                    poolBuilder.advance();
+                  }
+                  allDocs.remove(page);
+                }
+              });
 
-            Vec vec = feat.features();
-            vec = VecTools.concat(vec, new ArrayVec(0.0));
+          List<PageAndWight> newRes = new ArrayList<>(res);
+          final int[] tmpAdded = {0};
+          int[] cnt = {0};
+          allDocs
+              .entrySet()
+              .stream()
+              .sorted(Comparator.comparingDouble(e -> -model.trans(e.getValue().features()).get(0)))
+              .forEach(e -> {
+                Page page = e.getKey();
+                Features feat = e.getValue();
+                if (page == PlainPage.EMPTY_PAGE) {
+                  return;
+                }
 
-            if (cnt[0] < FILTER_SIZE) {
-              synchronized (poolBuilder) {
-                poolBuilder.accept(new QURLItem(page, query),
-                    vec, metas);
-              }
-              if (added.get() - tmpAdded < SAVE_SIZE) {
-                added.incrementAndGet();
-                newRes.add(new PageAndWight(page.uri().toString(), 0));
-              }
-              cnt[0]++;
-            }
-          });
-      newData.add(new QueryAndResults(query.text(), newRes));
-    });
+                Vec vec = feat.features();
+                vec = VecTools.concat(vec, new ArrayVec(0.0));
+
+                if (cnt[0] < FILTER_SIZE) {
+                  synchronized (poolBuilder) {
+                    poolBuilder.accept(new QURLItem(page, query),
+                        vec, metas);
+                  }
+                  if (tmpAdded[0] < SAVE_SIZE) {
+                    tmpAdded[0]++;
+                    newRes.add(new PageAndWight(page.uri().toString(), 0));
+                  }
+                  cnt[0]++;
+                }
+              });
+          added.addAndGet(tmpAdded[0]);
+          newData.add(new QueryAndResults(query.text(), newRes));
+        });
 
     saveNewData(newData.toArray(new QueryAndResults[0]), iteration + 1);
 
@@ -168,30 +189,6 @@ public class FilterPoolBuilder extends PoolBuilder {
       DataTools.writePoolTo(pool, Files.newBufferedWriter(dir.resolve("filter.pool")));
     } catch (IOException e) {
       System.err.println(e.getMessage());
-    }
-  }
-
-  private void accept(Builder<QURLItem> poolBuilder, Page page, Query query, Features feat,
-      double target) {
-    if (page == PlainPage.EMPTY_PAGE) {
-      return;
-    }
-    synchronized (poolBuilder) {
-      poolBuilder.accept(new QURLItem(page, query));
-      poolBuilder
-          .features()
-          .forEach(
-              fs -> {
-                if (fs instanceof TargetFS) {
-                  ((TargetFS) fs).acceptTargetValue(target);
-                } else if (fs instanceof FilterFeatures) {
-                  ((FilterFeatures) fs).withBody(feat.features(SECTION).get(0));
-                  ((FilterFeatures) fs).withLink(feat.features(LINK).get(0));
-                  ((FilterFeatures) fs).withTitle(feat.features(TITLE).get(0));
-                }
-              }
-          );
-      poolBuilder.advance();
     }
   }
 
