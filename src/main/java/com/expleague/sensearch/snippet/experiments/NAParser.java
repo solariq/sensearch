75eec32a57ae25570a82b3d3598b22e1997724a4
alvinmax <alvinmax@mail.ru>
2019-04-10 15:02:56 +0300

    changes for server

--- src/main/java/com/expleague/sensearch/snippet/experiments/NAParser.java
+++ src/main/java/com/expleague/sensearch/snippet/experiments/NAParser.java
@@ -31,11 +31,16 @@ public class NAParser {
   }
 
   public static void main(String[] args) throws Exception {
-    byte[] jsonData = Files.readAllBytes(Paths.get("./resources/nq-train-sample.json"));
+    if (args == null || args.length != 3) {
+      System.err.println("Wrong arguments. Usage : <path to na json> <action : 0 - data for ranking, 1 - data for snippets> <path to save data>");
+      return;
+    }
+    byte[] jsonData = Files.readAllBytes(Paths.get(args[0]));
 
     ObjectMapper objectMapper = new ObjectMapper();
 //    objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
     Result[] results = objectMapper.readValue(jsonData, Result[].class);
+    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
     List<Data> datas = new ArrayList<>();
     List<QueryAndResults> queryAndResults = new ArrayList<>();
@@ -43,46 +48,56 @@ public class NAParser {
     for (int i = 0; i < results.length; i++) {
       Result result = results[i];
 
-      { // for learning
-        String query = result.getQuestion_text();
-        String uri = createUriForTitle(result.getDocument_title()).toString();
-        PageAndWeight pw = new PageAndWeight(uri, 1.0);
-        queryAndResults.add(new QueryAndResults(query, Collections.singletonList(pw)));
-      }
+      switch (args[1]) {
+        case "0" :
+          { // for learning
+            String query = result.getQuestion_text();
+            String uri = createUriForTitle(result.getDocument_title()).toString();
+            PageAndWeight pw = new PageAndWeight(uri, 1.0);
+            queryAndResults.add(new QueryAndResults(query, Collections.singletonList(pw)));
+          }
+          break;
+        case "1":
+          if (result.getAnnotations().size() > 0
+                  && result.getAnnotations().get(0).getShort_answers().size() > 0) {
+            String title = result.getDocument_title();
+            String query = result.getQuestion_text();
+            final String html = result.getDocument_html();
+            byte[] htmlBytes = html.getBytes();
 
-      if (result.getAnnotations().size() > 0
-          && result.getAnnotations().get(0).getShort_answers().size() > 0) {
-        String title = result.getDocument_title();
-        String query = result.getQuestion_text();
-        final String html = result.getDocument_html();
-        byte[] htmlBytes = html.getBytes();
+            LongAnswer longAnswer = result.getAnnotations().get(0).getLong_answer();
+            final int lLong = longAnswer.getStart_byte();
+            final int rLong = longAnswer.getEnd_byte();
+            byte[] longAnsBytes = Arrays.copyOfRange(htmlBytes, lLong, rLong);
+            String longAnsString = eraseLinks(new String(longAnsBytes));
 
-        LongAnswer longAnswer = result.getAnnotations().get(0).getLong_answer();
-        final int lLong = longAnswer.getStart_byte();
-        final int rLong = longAnswer.getEnd_byte();
-        byte[] longAnsBytes = Arrays.copyOfRange(htmlBytes, lLong, rLong);
-        String longAnsString = eraseLinks(new String(longAnsBytes));
+            ShortAnswer shortAnswer = result.getAnnotations().get(0).getShort_answers().get(0);
+            final int l = shortAnswer.getStart_byte();
+            final int r = shortAnswer.getEnd_byte();
+            byte[] ans = Arrays.copyOfRange(htmlBytes, l, r);
+            String shortAnsString = eraseLinks(new String(ans));
 
-        ShortAnswer shortAnswer = result.getAnnotations().get(0).getShort_answers().get(0);
-        final int l = shortAnswer.getStart_byte();;
-        final int r = shortAnswer.getEnd_byte();
-        byte[] ans = Arrays.copyOfRange(htmlBytes, l, r);;
-        String shortAnsString = eraseLinks(new String(ans));
+            Data data = new Data();
+            data.setTitle(title);
+            data.setQuery(query);
+            data.setShort_answer(shortAnsString);
+            data.setLong_answer(longAnsString);
 
-        Data data = new Data();
-        data.setTitle(title);
-        data.setQuery(query);
-        data.setShort_answer(shortAnsString);
-        data.setLong_answer(longAnsString);
-
-        datas.add(data);
+            datas.add(data);
+          }
+        default:
+            return;
       }
     }
 
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-    mapper.writeValue(new File("./src/main/java/com/expleague/sensearch/snippet/experiments/data.json"), datas);
-    mapper.writeValue(new File("./PoolData/na.json"), queryAndResults);
+    switch (args[1]) {
+      case "0" :
+        objectMapper.writeValue(new File(args[2]), queryAndResults);
+        break;
+      case "1":
+        objectMapper.writeValue(new File(args[2]), datas);
+        break;
+      default:
+    }
   }
 }
