53da2e3fdcda9819d440cfed891839d0484fe30f
Vsevolod Stepanov <tehnar5@gmail.com>
2019-04-23 16:29:41 +0300

    Fix Joom csv data transforming

--- src/main/java/com/expleague/sensearch/experiments/joom/JoomCsvTransformer.java
+++ src/main/java/com/expleague/sensearch/experiments/joom/JoomCsvTransformer.java
@@ -8,7 +8,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,26 +22,39 @@ public class JoomCsvTransformer {
     CSVParser csvRecords =
         CSVParser.parse(originalDataPath, Charset.forName("UTF-8"), CSVFormat.DEFAULT);
 
-    Map<String, List<QueryAndResults.PageAndWeight>> joomQueries = new HashMap<>();
+    Map<String, Map<String, Integer>> joomQueries = new HashMap<>();
     for (CSVRecord record : csvRecords) {
+      if (record.size() != 3) {
+        throw new RuntimeException(record.toString());
+      }
+
       String query = record.get(0);
       String id = record.get(1);
       int purchases = Integer.parseInt(record.get(2));
 
-      joomQueries.putIfAbsent(query, new ArrayList<>());
-      joomQueries
-          .get(query)
-          .add(new PageAndWeight(JoomUtils.uriForId(id).toString(), Math.log(purchases + 2)));
-      if (record.size() != 3) {
-        throw new RuntimeException(record.toString());
-      }
+      joomQueries.putIfAbsent(query, new HashMap<>());
+      Map<String, Integer> map = joomQueries.get(query);
+      map.put(id, map.getOrDefault(id, 0) + purchases);
+      //      new PageAndWeight(JoomUtils.uriForId(id).toString(), Math.log(purchases + 2)));
     }
 
     List<QueryAndResults> data =
         joomQueries
             .entrySet()
             .stream()
-            .map(x -> new QueryAndResults(x.getKey(), x.getValue()))
+            .map(
+                x ->
+                    new QueryAndResults(
+                        x.getKey(),
+                        x.getValue()
+                            .entrySet()
+                            .stream()
+                            .map(
+                                y ->
+                                    new PageAndWeight(
+                                        JoomUtils.uriForId(y.getKey()).toString(),
+                                        Math.log(y.getValue() + 2)))
+                            .collect(Collectors.toList())))
             .collect(Collectors.toList());
     new ObjectMapper()
         .writeValue(Files.newBufferedWriter(convertedJsonDataPath, StandardCharsets.UTF_8), data);
