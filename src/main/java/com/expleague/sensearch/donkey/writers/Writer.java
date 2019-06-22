e188247abe628bb81f8efff89ede24618dbfe2ef
SandMV <sandulmv@yandex.ru>
2019-06-23 01:31:22 +0300

    Fixed 'Writer' interface

--- src/main/java/com/expleague/sensearch/donkey/writers/Writer.java
+++ src/main/java/com/expleague/sensearch/donkey/writers/Writer.java
@@ -2,8 +2,7 @@ package com.expleague.sensearch.donkey.writers;
 
 import java.io.Closeable;
 import java.io.Flushable;
-import java.io.IOException;
 
 public interface Writer<T> extends Closeable, Flushable {
-  public void write(T object) throws IOException;
+  void write(T object);
 }
