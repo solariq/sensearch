1391978a1928983221c74b6f105a631f2b23030d
SandMV <sandulmv@yandex.ru>
2019-06-22 23:42:47 +0300

    Added throwing 'IOException' from write method

--- src/main/java/com/expleague/sensearch/donkey/writers/Writer.java
+++ src/main/java/com/expleague/sensearch/donkey/writers/Writer.java
@@ -2,7 +2,8 @@ package com.expleague.sensearch.donkey.writers;
 
 import java.io.Closeable;
 import java.io.Flushable;
+import java.io.IOException;
 
 public interface Writer<T> extends Closeable, Flushable {
-  public void write(T object);
+  public void write(T object) throws IOException;
 }
