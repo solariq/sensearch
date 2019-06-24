9e7a1c7e12c926d4fd5af7c19852866cf73e5f39
SandMV <sandulmv@yandex.ru>
2019-06-24 20:24:56 +0300

    Added 'SequentialLinkReader'

--- src/main/java/com/expleague/sensearch/donkey/readers/SequentialLinkReader.java
+++ src/main/java/com/expleague/sensearch/donkey/readers/SequentialLinkReader.java
@@ -1,5 +1,39 @@
 package com.expleague.sensearch.donkey.readers;
 
-public class SequentialLinkReader {
+import com.expleague.sensearch.protobuf.index.IndexUnits.Page.Link;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class SequentialLinkReader implements Reader<Link> {
+
+  private final Path inputPath;
+  private final InputStream inputStream;
+
+  public SequentialLinkReader(Path inputPath) {
+    this.inputPath = inputPath;
+    try {
+      this.inputStream = new BufferedInputStream(Files.newInputStream(inputPath));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Link read() {
+    Link link;
+    try {
+      link = Link.parseDelimitedFrom(inputStream);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return link;
+  }
+
+  @Override
+  public void close() throws IOException {
+    inputStream.close();
+  }
 }
