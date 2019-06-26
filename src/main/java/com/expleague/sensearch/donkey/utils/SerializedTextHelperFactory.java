f7d9c93bff8d3fc9d421141f5d151e8eb4498ae8
SandMV <sandulmv@yandex.ru>
2019-06-27 01:42:48 +0300

    Replaced 'SerializedTextHelper' with factory

---
+++ src/main/java/com/expleague/sensearch/donkey/utils/SerializedTextHelperFactory.java
@@ -0,0 +1,56 @@
+package com.expleague.sensearch.donkey.utils;
+
+import com.expleague.sensearch.donkey.randomaccess.RandomAccess;
+import com.expleague.sensearch.protobuf.index.IndexUnits.Page.SerializedText;
+import com.expleague.sensearch.protobuf.index.IndexUnits.Term;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import java.util.stream.IntStream;
+
+public class SerializedTextHelperFactory {
+
+  private final TIntIntMap termIdToLemmaIdMap;
+
+  public SerializedTextHelperFactory(RandomAccess<Term> termIndex) {
+    termIdToLemmaIdMap = new TIntIntHashMap();
+    termIndex.forEach(term -> termIdToLemmaIdMap.put(term.getId(), term.getLemmaId()));
+  }
+
+  public SerializedTextHelper helper(SerializedText serializedText) {
+    return new SerializedTextHelper(serializedText);
+  }
+
+  public class SerializedTextHelper {
+
+    private final SerializedText serializedText;
+    private final int[] tokenIds;
+
+    private SerializedTextHelper(SerializedText serializedText) {
+      this.serializedText = serializedText;
+      this.tokenIds = serializedText.getTokenIdsList().stream()
+          .mapToInt(Integer::intValue)
+          .toArray();
+    }
+
+    public SerializedText serializedText() {
+      return serializedText;
+    }
+
+    public int[] tokenIds() {
+      return tokenIds;
+    }
+
+    public IntStream termIdsStream() {
+      return IntStream.of(tokenIds)
+          .filter(TokenParser::isWord)
+          .map(TokenParser::toId);
+    }
+
+    public IntStream lemmaIdsStream() {
+      return IntStream.of(tokenIds)
+          .filter(TokenParser::isWord)
+          .map(TokenParser::toId)
+          .map(termIdToLemmaIdMap::get);
+    }
+  }
+}
