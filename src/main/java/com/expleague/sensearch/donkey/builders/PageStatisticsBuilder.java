724a1c00bd4410c7ec49ec82b7b098b558b5d2ee
SandMV <sandulmv@yandex.ru>
2019-07-13 21:41:44 +0300

    Move 'PageStatisticsBuilder' to outter class

---
+++ src/main/java/com/expleague/sensearch/donkey/builders/PageStatisticsBuilder.java
@@ -0,0 +1,166 @@
+package com.expleague.sensearch.donkey.builders;
+
+import com.expleague.sensearch.donkey.utils.SerializedTextHelperFactory;
+import com.expleague.sensearch.donkey.utils.SerializedTextHelperFactory.SerializedTextHelper;
+import com.expleague.sensearch.protobuf.index.IndexUnits.Page;
+import com.expleague.sensearch.protobuf.index.IndexUnits.Page.Link;
+import com.expleague.sensearch.protobuf.index.IndexUnits.PageStatistics;
+import com.google.common.annotations.VisibleForTesting;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.TIntLongMap;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import gnu.trove.map.hash.TIntLongHashMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+import gnu.trove.set.TLongSet;
+import gnu.trove.set.hash.TLongHashSet;
+import java.util.stream.IntStream;
+
+@VisibleForTesting
+public class PageStatisticsBuilder {
+
+  // index-wise statistics
+  private int titlesCount;
+  private int titleTermsCount;
+  private int contentTermsCount;
+  private int linkTermsCount;
+  private int incomingLinksCount;
+  private int outgoingLinksCount;
+  private int targetTitleTermsCount;
+
+  // term-wise statistics
+  private final TIntLongMap wordFrequencyMap = new TIntLongHashMap();
+  private final BigramsFreqMapAccumulator bigramFreqAcc = new BigramsFreqMapAccumulator();
+
+  private final TLongSet knownTargets = new TLongHashSet();
+  private final TLongSet uniqueTargets = new TLongHashSet();
+  private final TLongSet uniqueReferrers = new TLongHashSet();
+
+  private final SerializedTextHelperFactory textHelperFactory;
+  private final long pageId;
+
+  public PageStatisticsBuilder(SerializedTextHelperFactory textHelperFactory, long pageId) {
+    this.textHelperFactory = textHelperFactory;
+    this.pageId = pageId;
+  }
+
+  public synchronized void addTarget(Page page) {
+    // this condition is always true for wikipedia but
+    // we want to determine the same behaviour for all possible collection
+    // not only for wikipedia
+    if (page.getRootId() != page.getPageId()) {
+      return;
+    }
+
+    if (knownTargets.contains(page.getPageId())) {
+      return;
+    }
+
+    if (page.getIncomingLinksList().stream().anyMatch(l -> l.getSourcePageId() == pageId)) {
+      targetTitleTermsCount += (int) textHelperFactory.helper(page.getTitle())
+          .termIdsStream()
+          .count();
+    }
+  }
+
+  public synchronized void addPage(Page page) {
+    if (page.getRootId() != pageId) {
+      return;
+    }
+
+    incomingLinksCount += (int) page.getIncomingLinksList().stream()
+        .mapToLong(Link::getSourcePageId)
+        .peek(uniqueReferrers::add)
+        .count();
+    outgoingLinksCount += (int) page.getOutgoingLinksList().stream()
+        .mapToLong(Link::getTargetPageId)
+        .peek(uniqueTargets::add)
+        .count();
+    titlesCount++;
+
+    SerializedTextHelper titleHelper = textHelperFactory.helper(page.getTitle());
+    processWordStream(titleHelper.lemmaIdsStream());
+    titleTermsCount += processWordStream(titleHelper.termIdsStream());
+
+    SerializedTextHelper contentHelper = textHelperFactory.helper(page.getContent());
+    processWordStream(contentHelper.lemmaIdsStream());
+    contentTermsCount += processWordStream(contentHelper.termIdsStream());
+
+    linkTermsCount += page.getIncomingLinksList().stream()
+        .flatMapToInt(l -> textHelperFactory.helper(l.getText()).termIdsStream())
+        .count();
+  }
+
+  /**
+   * Calculates word and bigram frequencies
+   *
+   * @param wordStream stream of word ids to be processed
+   * @return count of the elements of the stream
+   */
+  private int processWordStream(IntStream wordStream) {
+    int wordCount = (int) wordStream.sequential()
+        .peek(bigramFreqAcc::accumulate)
+        .peek(id -> wordFrequencyMap.adjustOrPutValue(id, 1, 1))
+        .count();
+    bigramFreqAcc.skipPreviousWord();
+
+    return wordCount;
+  }
+
+  public PageStatistics build() {
+    PageStatistics.Builder builder = PageStatistics.newBuilder();
+    builder.setPageId(pageId);
+
+    builder.setTitlesCount(titlesCount);
+    builder.setTitleTermsCount(titleTermsCount);
+    builder.setContentTermsCount(contentTermsCount);
+
+    builder.setLinkTermsCount(linkTermsCount);
+
+    builder.setIncomingLinksCount(incomingLinksCount);
+    builder.setTargetTitleTermsCount(targetTitleTermsCount);
+    builder.setOutgoingLinksCount(outgoingLinksCount);
+
+    builder.setUniqueReferrersCount(uniqueReferrers.size());
+    builder.setUniqueTargetsCount(uniqueTargets.size());
+
+    // TODO: complete build
+
+    return builder.build();
+  }
+
+  static class BigramsFreqMapAccumulator {
+
+    private int previousWordId;
+    private boolean hasPreviousWord;
+
+    private final TIntObjectMap<TIntIntMap> bigramsFrequencyMap;
+
+    BigramsFreqMapAccumulator(TIntObjectMap<TIntIntMap> bigramsFrequencyMap) {
+      this.bigramsFrequencyMap = bigramsFrequencyMap;
+    }
+
+    BigramsFreqMapAccumulator() {
+      this(new TIntObjectHashMap<>());
+    }
+
+    void accumulate(int wordId) {
+      if (!hasPreviousWord) {
+        hasPreviousWord = true;
+        previousWordId = wordId;
+      }
+
+      bigramsFrequencyMap.putIfAbsent(previousWordId, new TIntIntHashMap());
+      bigramsFrequencyMap.get(previousWordId).adjustOrPutValue(wordId, 1, 1);
+      previousWordId = wordId;
+    }
+
+    void skipPreviousWord() {
+      hasPreviousWord = false;
+    }
+
+    TIntObjectMap<TIntIntMap> bigramsFrequencyMap() {
+      return bigramsFrequencyMap;
+    }
+  }
+}
