d8e4d459fd56012b44fb85c7961cd2efa3abcd74
SandMV <sandulmv@yandex.ru>
2019-07-13 21:42:42 +0300

    Move 'PageStatisticsBuilder' to outter class part 2

--- src/main/java/com/expleague/sensearch/donkey/builders/PageStatisticsBuilderFactory.java
+++ src/main/java/com/expleague/sensearch/donkey/builders/PageStatisticsBuilderFactory.java
@@ -1,5 +1,17 @@
 package com.expleague.sensearch.donkey.builders;
 
+import com.expleague.sensearch.donkey.utils.SerializedTextHelperFactory;
+
 public class PageStatisticsBuilderFactory {
+
+  private final SerializedTextHelperFactory helperFactory;
+
+  public PageStatisticsBuilderFactory(SerializedTextHelperFactory helperFactory) {
+    this.helperFactory = helperFactory;
+  }
+
+  public PageStatisticsBuilder builder(long pageId) {
+    return new PageStatisticsBuilder(helperFactory, pageId);
+  }
 
 }
